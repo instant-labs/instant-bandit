dc7374063f4125bcdc3eafe3db364476ef50c28f
Greg Dingle <gregdingle@gmail.com>
2021-06-09 12:51:42 -0700

    type change

--- pages/api/probabilities.ts
+++ pages/api/probabilities.ts
@@ -2,19 +2,22 @@ import { NextApiRequest, NextApiResponse } from "next"
 
 // Defining Variant and Probability here in case they're intended to become
 // more strongly typed
-type Variant = string;
-type Probability = number;
+type Variant = string
+type Probability = number
 export type ProbabilityMap = Record<Variant, Probability>
 
 interface ProbabilityResponse {
-  experimentId: string | string[];
-  name: string;
+  experimentId: string
+  name: string
   probabilities: ProbabilityMap
 }
 
-export default (req: NextApiRequest, res: NextApiResponse<ProbabilityResponse>) => {
+export default (
+  req: NextApiRequest,
+  res: NextApiResponse<ProbabilityResponse>
+) => {
   // TODO: finish API
-  const experimentId = req.query.experimentId
+  const experimentId = req.query.experimentId as string
   res.status(200).json({
     experimentId,
     name: "probabilities",
