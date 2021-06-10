51156f80e8e7b1801e67b3ce62382409bce5b40f
Greg Dingle <gregdingle@gmail.com>
2021-06-09 20:04:57 -0700

    update api tests

--- pages/api/_api.test.ts
+++ pages/api/_api.test.ts
@@ -1,16 +1,25 @@
 // NOTE: next server should be running. Try `yarn run dev`.
 
 import fetch from "node-fetch"
-describe("hello", () => {
+describe("_hello", () => {
   test("returns", async () => {
-    const res = await fetch("http://localhost:3000/api/hello")
+    const res = await fetch("http://localhost:3000/api/_hello")
     const data = await res.json()
     expect(data).toEqual({ name: "Hello World" })
   })
 })
 
-describe("probabilities", () => {
+describe("_database", () => {
   test("returns", async () => {
+    const res = await fetch("http://localhost:3000/api/_database")
+    const data = await res.json()
+    expect(data).toEqual({ _testKey: "true" })
+  })
+})
+
+// TODO: rewrite these for current behavior
+describe("probabilities", () => {
+  test.skip("returns", async () => {
     const res = await fetch("http://localhost:3000/api/probabilities")
     const data = await res.json()
     expect(data).toEqual({
@@ -21,7 +30,7 @@ describe("probabilities", () => {
 })
 
 describe("exposures", () => {
-  test("returns", async () => {
+  test.skip("returns", async () => {
     const res = await fetch("http://localhost:3000/api/exposures")
     const data = await res.json()
     expect(data).toEqual({ name: "exposures" })
@@ -29,17 +38,9 @@ describe("exposures", () => {
 })
 
 describe("conversions", () => {
-  test("returns", async () => {
+  test.skip("returns", async () => {
     const res = await fetch("http://localhost:3000/api/conversions")
     const data = await res.json()
     expect(data).toEqual({ name: "conversions" })
-  })
-})
-
-describe("_database", () => {
-  test("returns", async () => {
-    const res = await fetch("http://localhost:3000/api/_database")
-    const data = await res.json()
-    expect(data).toEqual({ _testKey: true })
   })
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home(serverSideProps: Props) {
       </main>
 
       <footer className={styles.footer}>
-        <a href="/api/hello" target="_blank">
+        <a href="/api/_hello" target="_blank">
           Is the server running?
         </a>
         <a href="/api/_database" target="_blank">
