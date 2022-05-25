c596f5748ea08e40b630a3de0ede66164eb9b4da
Jason Scharf <jasonlscharf@gmail.com>
2022-05-25 13:30:53 -0700

    Fix failing tests due to new pValue

--- pages/api/_database.ts
+++ pages/api/_database.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { db } from "../../lib/db"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await db().set("_testKey", true)
+  await db().set("_testKey", "true")
   const _testKey = await db().get("_testKey")
   res.status(200).json({ _testKey })
 }
