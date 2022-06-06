45de83f7574e219cb04cc8ec8c2a2f63bed949f5
Jason Scharf <jasonlscharf@gmail.com>
2022-06-06 15:41:35 -0700

    Add a Redis instance for dev

---
+++ lib/server/environment.ts
@@ -0,0 +1,26 @@
+// These are overlaid with process variables, if set
+const defaults = {
+  IB_MODE: process.env.NODE_ENV,
+  IB_ORIGINS_WHITELIST: "",
+  IB_REDIS_HOST: "redis",
+  IB_REDIS_PORT: 6379,
+  IB_STATIC_SITES_PATH: "./",
+  IB_MAX_METRICS_PAYLOAD_LEN: 1024,
+}
+
+const funcs = {
+  isDev: () => env.IB_MODE === "development",
+  isTest: () => env.IB_MODE === "test",
+  isProduction: () => env.IB_MODE === "production",
+}
+
+export type Environment = typeof defaults & typeof funcs
+const env: Environment = Object.assign({}, defaults, funcs)
+
+// Pull from actual environment vars, or use the defaults above
+Object
+  .keys(defaults)
+  .filter(key => typeof process.env[key] !== "undefined")
+  .forEach(key => (env as any)[key] = process.env[key]);
+
+export default env
