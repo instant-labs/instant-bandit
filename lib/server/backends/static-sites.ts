030bcdcb183f0867a3c01e847e05dd777b7a4b13
Jason Scharf <jasonlscharf@gmail.com>
2022-06-14 08:41:27 -0700

    Add static sites helper (#26)
    * Add static sites helper
    
    * Style tweaks re: PR feedback

--- lib/server/backends/static-sites.ts
+++ lib/server/backends/static-sites.ts
@@ -1,8 +1,14 @@
+import fs from "fs"
+import path from "path"
+import { cwd } from "process"
+
 import env from "../environment"
 import { ModelsBackend, ValidatedRequest } from "../server-types"
 import { Site } from "../../models"
 import { DEFAULT_SITE } from "../../defaults"
-import { DEMO_SITE } from "../../sites"
+import { TimerLike } from "../../types"
+
+const asyncfs = fs.promises
 
 
 export type JsonSiteBackendOptions = {
@@ -10,13 +16,11 @@ export type JsonSiteBackendOptions = {
   /** Base path to load sites from */
   basePath: string,
 
-  /** Minimum time in ms to wait before reloading a site from disk */
+  /**
+   * Minimum time in ms to wait before reloading a site from disk.
+   * Set to -1 for no reloading.
+   */
   refreshInterval: number,
-
-  /** Map of site names to local paths */
-  sites: {
-    [siteName: string]: string,
-  }
 }
 
 /**
@@ -24,24 +28,90 @@ export type JsonSiteBackendOptions = {
  */
 export function getStaticSiteBackend(initOptions: Partial<JsonSiteBackendOptions> = {}): ModelsBackend {
   const options = Object.assign({}, DEFAULT_STATIC_SITE_BACKEND_OPTIONS, initOptions)
-  return {
-    async getSiteConfig(req: ValidatedRequest): Promise<Site> {
+  let sites: { [name: string]: Site } = {}
+  let lastScan = 0
 
-      // STUB
-      if (req.siteName === "demo") {
-        return DEMO_SITE
-      } else {
-        return DEFAULT_SITE
+  function logError(err: Error) {
+    console.warn(`[IB] Error scanning sites: ${err}`)
+  }
+
+  return {
+    async connect() {
+      try {
+        sites = await scanSites(env.IB_STATIC_SITES_PATH, sites, lastScan)
+      } catch (err) {
+        logError(err)
       }
+    },
+
+    async getSiteConfig(req: ValidatedRequest): Promise<Site> {
+      const { siteName } = req
+      const { refreshInterval } = options
+      const now = new Date().getTime()
+
+      // Only scan the folder for updated sites if the configured interval has elapsed
+      const useRefreshInterval = refreshInterval > -1
+      const intervalElapsed = (now - lastScan > refreshInterval)
+
+      if (useRefreshInterval && intervalElapsed) {
+
+        // Don't hold up the show.
+        scanSites(env.IB_STATIC_SITES_PATH, sites, lastScan)
+          .then(scanned => sites = scanned)
+          .then(() => lastScan = now)
+          .catch(logError)
+      }
+
+      const site = sites[siteName ?? DEFAULT_SITE.name]
+      if (site) {
+        return site
+      }
+      return DEFAULT_SITE
     }
   }
+}
+
+/**
+ * Scans the public sites folder, reloading any modified sites.
+ * Observes a configurable interval so as to not cause unnecessary IO.
+ */
+export async function scanSites(folder: string, sites: { [name: string]: Site } = {}, lastScan: number = 0) {
+  try {
+    await asyncfs.stat(folder)
+  } catch (err) {
+    console.warn(`[IB] Public sites folder does not exist`)
+    return sites
+  }
+
+  const files = await asyncfs.readdir(folder)
+  const jsonFiles = files.filter(name => name.endsWith(".json"))
+  const root = path.join(cwd(), folder)
+
+  for (const jsonFile of jsonFiles) {
+    const siteName = jsonFile.replace(".json", "")
+    const fullPath = path.join(root, jsonFile)
+    const stat = await asyncfs.stat(fullPath)
+    const { mtimeMs } = stat
+
+    if (mtimeMs < lastScan) {
+      continue
+    }
+
+    try {
+      const jsonBuffer = await asyncfs.readFile(fullPath)
+      const jsonStr = jsonBuffer.toString("utf-8")
+      const json = JSON.parse(jsonStr)
+
+      sites[siteName] = json
+    } catch (err) {
+      console.warn(`[IB] Error reading '${folder}/${jsonFile}': ${err}`)
+    }
+  }
+
+  return sites
 }
 
 export const DEFAULT_STATIC_SITE_BACKEND_OPTIONS: JsonSiteBackendOptions = {
   basePath: env.IB_STATIC_SITES_PATH,
   refreshInterval: 10 * 1000,
-  sites: {
-    "default": "default.json",
-    "demo": "demo.json",
-  }
 }
