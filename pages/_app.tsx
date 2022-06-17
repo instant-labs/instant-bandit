397517f3d5a51ca9ffa8c9e57c8d6138f15b1ea1
Jason Scharf <jasonlscharf@gmail.com>
2022-06-17 11:07:18 -0700

    Switch back to SSR in example page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,9 @@
-import { InstantBandit } from '../components/InstantBanditComponent';
-
 import '../styles/globals.css';
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <InstantBandit siteName={"demo"}>
       <Component {...pageProps} />
-    </InstantBandit>
   );
 }
 
