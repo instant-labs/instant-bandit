3bfc2aa936f3abf883b561382a417c1a698cc7ef
Jason Scharf <jasonlscharf@gmail.com>
2022-06-17 12:58:35 -0700

    Move endpoints impls back under pages/api

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -1,4 +1,63 @@
-import { createMetricsEndpoint, getBanditServer } from "../../lib/server/server-helpers";
+import { NextApiRequest, NextApiResponse } from "next";
+import { MetricsBatch } from "../../lib/models";
+import { getInternalDevServer } from "../../lib/server/server-internal";
+import { InstantBanditHeaders } from "../../server";
+import { validateUserRequest } from "../../server";
+import { getSessionIdFromHeaders } from "../../server";
+import { ServerSession } from "../../server";
+import { InstantBanditServer } from "../../server";
 
-// Creates the metrics endpoint using the same helper method exposed to package consumers
-export default createMetricsEndpoint(getBanditServer());
+
+const MetricsEndpoint = createMetricsEndpoint(getInternalDevServer());
+export default MetricsEndpoint;
+
+/**
+ * Creates a Next.js endpoint for ingesting metrics.
+ * Remember to return the resulting endpoint function as your module's default export.
+ * See [metrics].ts.
+ */
+export function createMetricsEndpoint(server: InstantBanditServer) {
+
+  // This endpoint accepts POST requests bearing batches of metrics to ingest.
+  // In development environments, shows site metrics on GET
+  async function handleMetricsRequest(req: NextApiRequest, res: NextApiResponse) {
+    await server.init();
+
+    // TODO: Respond to CORS preflights
+
+    const { metrics, origins, sessions } = server;
+    const { method, headers } = req;
+
+
+    // No session? Politely do nothing.
+    const sid = await getSessionIdFromHeaders(headers as InstantBanditHeaders);
+    if (!sid || sid === "") {
+      res.status(200).json({ status: "OK" });
+      return;
+    }
+
+    if (method === "POST") {
+      const batch = req.body as MetricsBatch;
+      const validatedReq = await validateUserRequest({
+        allowedOrigins: origins,
+        headers,
+        url: req.url,
+        allowNoSession: true,
+        siteName: batch.site,
+      });
+
+      const session = await sessions.getOrCreateSession(validatedReq);
+      validatedReq.session = session as ServerSession;
+
+      await metrics.ingestBatch(validatedReq, req.body);
+      res.status(200).json({ status: "OK" });
+      return;
+
+    } else {
+      res.status(400);
+      return;
+    }
+  }
+
+  return handleMetricsRequest;
+}
--- pages/api/sites/[siteName].ts
+++ pages/api/sites/[siteName].ts
@@ -1,4 +1,69 @@
-import { createSiteEndpoint, getBanditServer } from "../../../lib/server/server-helpers";
+import { NextApiRequest, NextApiResponse } from "next";
 
-// Creates the site endpoint using the same helper method exposed to package consumers
-export default createSiteEndpoint(getBanditServer());
+import env from "../../../lib/server/environment";
+import { InstantBanditServer } from "../../../server";
+import { DEFAULT_SITE } from "../../../lib/defaults";
+import { exists } from "../../../lib/utils";
+import { validateUserRequest } from "../../../server";
+import { getInternalDevServer } from "../../../lib/server/server-internal";
+
+
+const SiteEndpoint = createSiteEndpoint(getInternalDevServer());
+export default SiteEndpoint;
+
+/**
+ * Creates a Next.js endpoint for serving site configurations.
+ * Remember to return the resulting endpoint function as your module's default export.
+ * See [siteName].ts.
+ */
+export function createSiteEndpoint(server: InstantBanditServer) {
+
+  // This endpoint serves site configurations by name and creates user sessions.
+  // Site are hydrated with variant probabilities inlined.
+  // If a site is not found, the default site is returned.
+  // If the user does not have a session, one is created.
+  // Session IDs are transmitted via a 1st-party cookie.
+
+  async function handleSiteRequest(req: NextApiRequest, res: NextApiResponse) {
+    await server.init();
+
+    // TODO: Respond to CORS preflights
+
+    const { siteName: siteNameParam } = req.query;
+    const { origins } = server;
+    const { url, headers } = req;
+
+    let siteName = DEFAULT_SITE.name;
+    if (exists(siteNameParam)) {
+      if (Array.isArray(siteNameParam)) {
+        siteName = siteNameParam.join("");
+      } else {
+        siteName = siteNameParam;
+      }
+    }
+
+    const validatedRequest = await validateUserRequest({
+      url,
+      headers,
+      allowedOrigins: origins,
+      siteName,
+
+      // No session required for a site request. One will be created if need be.
+      allowNoSession: true,
+    });
+
+    const { site, responseHeaders } = await server.getSite(validatedRequest);
+
+    // Relay headers
+    Object.keys(responseHeaders)
+      .forEach(header => res.setHeader(header, responseHeaders[header] ?? ""));
+
+    if (env.isDev()) {
+      res.status(200).send(JSON.stringify(site, null, 2));
+    } else {
+      res.status(200).send(site);
+    }
+  }
+
+  return handleSiteRequest;
+}
